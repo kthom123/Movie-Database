0486597fdb4310709d7cb7e586b75a78128844af
Kyle Thomas <kylejwthomas@gmail.com>
2024-06-27 11:00:19 +0900

    Edit move class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,5 +5,20 @@ namespace Movie_Database.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int Rating { get; set; }
     }
 }
