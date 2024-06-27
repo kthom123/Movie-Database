bdde0e268c227c3113cb1f160864df4dcd0892a7
Kyle Thomas <kylejwthomas@gmail.com>
2024-06-27 14:10:30 +0900

    Added data folder

---
+++ Data/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_Database.Models;
+
+namespace Movie_Database.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
